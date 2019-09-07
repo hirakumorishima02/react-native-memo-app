75f948040aa7bb63ec2d2857a2fab9aa0ebadde5
hirakumorishima02 <business.ujinchu@gmail.com>
2019-09-07 13:24:35 +0900

    Style MemoDetailScreen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { StyleSheet, View } from 'react-native';
 
 import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Appbar />
-      <MemoListScreen />
+
+      <MemoDetailScreen />
     </View>
   );
 }
@@ -19,6 +21,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fffDF6',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 78,
+    paddingTop: 70,
   },
 });
